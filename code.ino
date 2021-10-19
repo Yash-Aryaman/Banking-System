ef53dd0d80757b3da017ea2dca9dd461cd5ef77e
Yash-Aryaman <92753516+Yash-Aryaman@users.noreply.github.com>
2021-10-19 15:51:35 +0530

    Update code.ino

--- code.ino
+++ code.ino
@@ -3,6 +3,78 @@ import mysql.connector as mysql
 db = mysql.connect(host="localhost",user="root",password="",database="college")
 command_handler = db.cursor(buffered=True)
 
+def teacher_session():
+    while 1:
+        print("")
+        print("Teacher's Menu")
+        print("1. Mark student register")
+        print("2. View register")
+        print("3. Logout")
+    
+        user_option = input(str("Option : "))
+        if user_option == "1":
+            print("")
+            print("Mark student register")
+            command_handler.execute("SELECT username FROM users WHERE privilege = 'student'")
+            records = command_handler.fetchall()
+            date    = input(str("Date : DD/MM/YYYY : "))
+            for record in records:
+                record = str(record).replace("'","")
+                record = str(record).replace(",","")
+                record = str(record).replace("(","")
+                record = str(record).replace(")","")
+                #Present | Absent | Late
+                status = input(str("Status for " + str(record) + "P/A/L : "))
+                query_vals = (str(record),date,status)
+                command_handler.execute("INSERT INTO attendance (username, date, status) VALUES(%s,%s,%s)",query_vals)
+                db.commit()
+                print(record + " Marked as " + status)
+        elif user_option == "2":
+            print("")
+            print("Viewing all student registers")
+            command_handler.execute("SELECT username, date, status FROM attendance")
+            records = command_handler.fetchall()
+            print("Displaying all registers")
+            for record in records:
+                print(record)
+        elif user_option == "3":
+            break
+        else:
+            print("No valid option was selected")
+
+def student_session(username):
+    while 1:
+        print("")
+        print("Student's Menu")
+        print("")
+        print("1. View Register")
+        print("2. Download Register")
+        print("3. Logout")
+
+        user_option = input(str("Option : "))
+        if user_option == "1":
+            print("Displaying register")
+            username = (str(username),)
+            command_handler.execute("SELECT date, username, status FROM attendance WHERE username = %s",username)
+            records = command_handler.fetchall()
+            for record in records:
+                print(record)
+        elif user_option == "2":
+            print("Downloading Register")
+            username = (str(username),)
+            command_handler.execute("SELECT date, username, status FROM attendance WHERE username = %s",username)
+            records = command_handler.fetchall()
+            for record in records:
+                with open("C:/Users/Johan Godinho/Desktop/register.txt", "w") as f:
+                    f.write(str(records)+"\n")
+                f.close()
+            print("All records saved")
+        elif user_option == "3":
+            break
+        else:
+            print("No valid option was selected")
+
+
 def admin_session():
     while 1:
         print("")
@@ -63,6 +135,30 @@ def admin_session():
         else:
             print("No valid option selected")
 
+def auth_student():
+    print("")
+    print("Student's Login")
+    print("")
+    username = input(str("Username : "))
+    password = input(str("Password : "))
+    query_vals = (username, password, "student")
+    command_handler.execute("SELECT username FROM users WHERE username = %s AND password = %s AND privilege = %s",query_vals)
+    if command_handler.rowcount <= 0:
+        print("Invalid login details")
+    else:
+        student_session(username)
+def auth_teacher():
+    print("")
+    print("Teacher's Login")
+    print("")
+    username = input(str("Username : "))
+    password = input(str("Password : "))
+    query_vals = (username, password)
+    command_handler.execute("SELECT * FROM users WHERE username = %s AND password = %s AND privilege = 'teacher'",query_vals)
+    if command_handler.rowcount <= 0:
+        print("Login not recognized")
+    else:
+        teacher_session()
 
 def auth_admin():
     print("")
@@ -88,9 +184,9 @@ def main():
 
         user_option = input(str("Option : "))
         if user_option == "1":
-            print("Student login")
+            auth_student()
         elif user_option == "2":
-            print("Teacher login")
+            auth_teacher()
         elif user_option == "3":
             auth_admin()
         else:
